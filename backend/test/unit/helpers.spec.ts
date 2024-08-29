4fe2a056f1608737eccbaa0f5a45ddd7abaa9ae5
ndbac <ryannguyen0303@gmail.com>
2024-08-29 23:18:31 +0700

    chore: add getBearerTokenFromRequest helper test

--- backend/test/unit/helpers.spec.ts
+++ backend/test/unit/helpers.spec.ts
@@ -1,4 +1,8 @@
-import { generateRandomString } from 'src/shared/helpers';
+import {
+  generateRandomString,
+  getBearerTokenFromRequest,
+} from 'src/shared/helpers';
+import { Request } from 'express';
 
 describe('generateRandomString', () => {
   it('should generate random string with correct legnth', () => {
@@ -14,5 +18,25 @@ describe('generateRandomString', () => {
   it('should generate random string with length = 0 if input length is negative number', () => {
     const random = generateRandomString(-1);
     expect(random.length).toStrictEqual(0);
+  });
+});
+
+describe('getBearerTokenFromRequest', () => {
+  it('should parse bearer token successfully', () => {
+    const req = {
+      headers: {
+        authorization: 'Bearer abcdefghijklmn',
+      },
+    };
+    const token = getBearerTokenFromRequest(req as Request);
+    expect(token).toStrictEqual('abcdefghijklmn');
+  });
+
+  it('should return undefined if no authorization header', () => {
+    const req = {
+      headers: {},
+    };
+    const token = getBearerTokenFromRequest(req as Request);
+    expect(token).toStrictEqual(undefined);
   });
 });
