e474990979e6280d936b94ac149d39e0dac71278
ndbac <ryannguyen0303@gmail.com>
2024-08-29 19:40:26 +0700

    fix: core model not found

--- backend/src/modules/shares/shares.core.module.ts
+++
@@ -1,9 +0,0 @@
-import { ShareRepository } from './shares.repository';
-import { Module } from '@nestjs/common';
-import { TypeOrmModule } from '@nestjs/typeorm';
-
-@Module({
-  imports: [TypeOrmModule.forFeature([ShareRepository])],
-  exports: [TypeOrmModule],
-})
-export class ShareCoreModule {}
