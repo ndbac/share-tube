5112299c54ecc7627e20c053a06b03473a5c838d
ndbac <ryannguyen0303@gmail.com>
2024-08-30 21:30:19 +0700

    chore: remove redundant codes

--- backend/src/pipes/general-validation.pipe.ts
+++ backend/src/pipes/general-validation.pipe.ts
@@ -1,13 +1,4 @@
-import {
-  ArgumentMetadata,
-  ValidationPipe,
-  ValidationPipeOptions,
-} from '@nestjs/common';
-import _ from 'lodash';
-
-interface ICustomParamValidationPipeOptions extends ValidationPipeOptions {
-  excludedTransformKeys: string[];
-}
+import { ValidationPipe } from '@nestjs/common';
 
 export const generalValidationPipe = new ValidationPipe({
   transform: true,
@@ -17,44 +8,4 @@ export const generalValidationPipe = new ValidationPipe({
     value: false,
   },
   validateCustomDecorators: true,
-});
-
-class CustomParamValidationPipe extends ValidationPipe {
-  excludedKeys: string[] = [];
-  constructor(opts: ICustomParamValidationPipeOptions) {
-    super(opts);
-    this.excludedKeys = opts.excludedTransformKeys;
-  }
-
-  async transform(value: any, metadata: ArgumentMetadata) {
-    const excludedValues = {};
-    this.excludedKeys.forEach((path) => {
-      _.set(excludedValues, path, _.get(value, path));
-      _.unset(value, path);
-    });
-    const afterTransformedValue = await super.transform(value, metadata);
-    this.excludedKeys.forEach((path) => {
-      _.set(afterTransformedValue, path, _.get(excludedValues, path));
-    });
-    return afterTransformedValue;
-  }
-}
-
-export const getCustomParamValidationPipe = (
-  options: Pick<ICustomParamValidationPipeOptions, 'excludedTransformKeys'>,
-) => {
-  return new CustomParamValidationPipe({
-    transform: true,
-    validationError: {
-      target: true,
-      value: false,
-    },
-    whitelist: true,
-    validateCustomDecorators: true,
-    ...options,
-  });
-};
-
-export const EXCLUDE_REQ_CTX_VALIDATION_PIPE = getCustomParamValidationPipe({
-  excludedTransformKeys: ['profile', 'user', 'url'],
 });
