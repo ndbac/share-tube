e338f37c1ddb3f49e646d2c7f1d20185f139a9e9
ndbac <ryannguyen0303@gmail.com>
2024-09-02 21:16:39 +0800

    chore: add axios service tests

--- frontend/src/services/axiosService.ts
+++ frontend/src/services/axiosService.ts
@@ -1,7 +1,7 @@
 import { IVideoShare } from "@/types";
 import axios from "axios";
 
-const axiosInstance = axios.create({
+export const axiosInstance = axios.create({
   baseURL: process.env.NEXT_PUBLIC_API_BASE_URL,
   headers: {
     "Content-Type": "application/json",
